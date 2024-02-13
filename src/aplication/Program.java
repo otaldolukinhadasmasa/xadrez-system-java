d726166fc34d6129c523de797f6dcd530252680b
Lucas h <lucashenrikjj@gmail.com>
2024-02-13 15:01:22 -0300

    Primeira classe: Posicao

---
+++ src/aplication/Program.java
@@ -0,0 +1,11 @@
+package aplication;
+
+import tabuleiro.Posicao;
+
+public class Program{
+    public static void main(String[] args) throws Exception {
+        
+        Posicao pos = new Posicao(3, 5);
+        System.out.println(pos);
+    }
+}
